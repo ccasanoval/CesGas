8d67705937cc7d03dbb735e8ca7f5235ce58cd84
Cesar Casanova <cesarcl@ext.inditex.com>
2025-05-14 17:23:25 +0200

    Update gradle and show station info on map

--- app/src/main/java/com/cesoft/cesgas/ui/common/MapCompo.kt
+++ app/src/main/java/com/cesoft/cesgas/ui/common/MapCompo.kt
@@ -3,15 +3,16 @@ package com.cesoft.cesgas.ui.common
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.drawable.Drawable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.viewinterop.AndroidView
 import com.cesoft.cesgas.R
@@ -36,69 +37,80 @@ fun MapCompo(
     doZoom: Boolean = false
 ) {
     //Without Scaffold the osmdroid map draws outside its AndroidView limits
-    Scaffold(modifier = modifier) { innerPadding ->
-        AndroidView(
-            factory = { mapView },
-            modifier = Modifier.padding(innerPadding)
-        ) { view ->
-            view.overlays.removeAll { true }
+    val selectedStation = remember { mutableStateOf<Station?>(null) }
 
-            val locationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(context), mapView)
-            locationOverlay.enableMyLocation()
-            view.overlays.add(locationOverlay)
-            view.controller.setCenter(locationOverlay.myLocation)
+    Column {
+        selectedStation.value?.let {
+            Text(" ************ ")
+            Text("${it.title}")
+            Text(stringResource(R.string.g95) + " : " + it.prices.G95 + "€")
+            Text(stringResource(R.string.goa) + " : " + it.prices.GOA + "€")
+        }
+        Scaffold(modifier = modifier) { innerPadding ->
+            AndroidView(
+                factory = { mapView },
+                modifier = Modifier.padding(innerPadding)
+            ) { view ->
+                view.overlays.removeAll { true }
 
-            var ss = stations.filter { it.location.latitude != 0.0 && it.location.latitude != 0.0 }
-            ss = when(productType) {
-                ProductType.G95 -> ss.map { it.copy(workingPrice = it.prices.G95 ?: 0f) }
-                ProductType.G98 -> ss.map { it.copy(workingPrice = it.prices.G98 ?: 0f) }
-                ProductType.GOA -> ss.map { it.copy(workingPrice = it.prices.GOA ?: 0f) }
-                ProductType.GOB -> ss.map { it.copy(workingPrice = it.prices.GOB ?: 0f) }
-                ProductType.GOC -> ss.map { it.copy(workingPrice = it.prices.GOC ?: 0f) }
-                ProductType.GLP -> ss.map { it.copy(workingPrice = it.prices.GLP ?: 0f) }
-                ProductType.GOAP -> ss.map { it.copy(workingPrice = it.prices.GOAP ?: 0f) }
-                else -> ss.map { it.copy(workingPrice = it.prices.G95 ?: 0f) }
-            }
-            ss = ss.filter { it.workingPrice > 0 }.sortedBy { it.workingPrice }
-            val MAX_STATIONS = 10
-            ss = ss.subList(0, MAX_STATIONS)
+                val locationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(context), mapView)
+                locationOverlay.enableMyLocation()
+                view.overlays.add(locationOverlay)
+                view.controller.setCenter(locationOverlay.myLocation)
 
-            val maxPrice = ss.maxOf { it.workingPrice }
-            val minPrice = ss.minOf { it.workingPrice }
-            val threePart = (maxPrice - minPrice)/3
+                var ss =
+                    stations.filter { it.location.latitude != 0.0 && it.location.latitude != 0.0 }
+                ss = when (productType) {
+                    ProductType.G95 -> ss.map { it.copy(workingPrice = it.prices.G95 ?: 0f) }
+                    ProductType.G98 -> ss.map { it.copy(workingPrice = it.prices.G98 ?: 0f) }
+                    ProductType.GOA -> ss.map { it.copy(workingPrice = it.prices.GOA ?: 0f) }
+                    ProductType.GOB -> ss.map { it.copy(workingPrice = it.prices.GOB ?: 0f) }
+                    ProductType.GOC -> ss.map { it.copy(workingPrice = it.prices.GOC ?: 0f) }
+                    ProductType.GLP -> ss.map { it.copy(workingPrice = it.prices.GLP ?: 0f) }
+                    ProductType.GOAP -> ss.map { it.copy(workingPrice = it.prices.GOAP ?: 0f) }
+                    else -> ss.map { it.copy(workingPrice = it.prices.G95 ?: 0f) }
+                }
+                ss = ss.filter { it.workingPrice > 0 }.sortedBy { it.workingPrice }
+                val maxStations = 10
+                if (ss.size > maxStations) ss = ss.subList(0, maxStations)
 
-            ss.map { s ->
-                val icon =
-                    if(s.workingPrice < minPrice + threePart)
-                        context.getDrawable(android.R.drawable.btn_star_big_on)
-                    else if(s.workingPrice > maxPrice - threePart)
-                        context.getDrawable(android.R.drawable.ic_lock_lock)
-                    else
-                        context.getDrawable(android.R.drawable.ic_lock_idle_lock)
-                val snippet = context.getString(R.string.price) + s.workingPrice
-                addMarker(
-                    mapView = view,
-                    geoPoint = GeoPoint(s.location.latitude, s.location.longitude),
-                    icon = icon,
-                    title = s.title,
-                    snippet = snippet,
-                    onClick = {
-                        //TODO: Show info window
-                        android.util.Log.e("AAA", "------------------ ${s.title} : ${s.hours} : ${s.prices.G95}")
-                    }
-                )
-            }
+                val maxPrice = ss.maxOf { it.workingPrice }
+                val minPrice = ss.minOf { it.workingPrice }
+                val threePart = (maxPrice - minPrice) / 3
 
-            view.addOnFirstLayoutListener { _, _, _, _, _ ->//v, left, top, right, bottom ->
-                //if (points.isEmpty()) {
+                ss.map { s ->
+                    val icon =
+                        if (s.workingPrice < minPrice + threePart)
+                            context.getDrawable(android.R.drawable.btn_star_big_on)
+                        else if (s.workingPrice > maxPrice - threePart)
+                            context.getDrawable(android.R.drawable.ic_lock_lock)
+                        else
+                            context.getDrawable(android.R.drawable.ic_lock_idle_lock)
+                    val snippet = context.getString(R.string.price) + s.workingPrice
+                    addMarker(
+                        mapView = view,
+                        geoPoint = GeoPoint(s.location.latitude, s.location.longitude),
+                        icon = icon,
+                        title = s.title,
+                        snippet = snippet,
+                        onClick = {
+                            selectedStation.value = s
+android.util.Log.e("AAA", "on marker click---------- ${s.title} : ${s.hours} : ${s.prices.G95}")
+                        }
+                    )
+                }
+
+                view.addOnFirstLayoutListener { _, _, _, _, _ ->//v, left, top, right, bottom ->
+                    //if (points.isEmpty()) {
                     //view.controller.setCenter(locationOverlay.myLocation)
                     //location?.let { view.controller.setCenter(GeoPoint(it.latitude, it.longitude)) }
-                //} else {
-                val gps = ss.map { GeoPoint(it.location.latitude, it.location.longitude) }
-                view.zoomToBoundingBox(BoundingBox.fromGeoPointsSafe(gps), false)
-                //view.controller.setCenter(locationOverlay.myLocation)
-                //}
-                view.invalidate()
+                    //} else {
+                    val gps = ss.map { GeoPoint(it.location.latitude, it.location.longitude) }
+                    view.zoomToBoundingBox(BoundingBox.fromGeoPointsSafe(gps), false)
+                    //view.controller.setCenter(locationOverlay.myLocation)
+                    //}
+                    view.invalidate()
+                }
             }
         }
     }
@@ -149,11 +161,13 @@ fun addMarker(
     marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
     marker.infoWindow = null
     marker.setOnMarkerClickListener { _, _ ->
+        //marker.showInfoWindow()
         onClick?.let {
             it()
             true
         } ?: run { false }
     }
+    //marker.setInfoWindow(MarkerInfoWindow())
 
     title?.let { marker.title = it }
     snippet?.let { marker.snippet = it }
